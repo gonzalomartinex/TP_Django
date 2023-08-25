cccff178e555f81c199cc50c065568398dae97cd
gonzalomartinex <129513137+gonzalomartinex@users.noreply.github.com>
2023-08-25 10:00:17 -0300

    delete de supermarket app
    Sacamos la app supermarket para mayor comodidad ya que funcionaba

--- PaginaDeStreaming/supermarket/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
-
-def titulo (request):
-    doc_externo=open("Documentos/Programacion/Django/proyecto1/supermarket/migrations/index.html")
