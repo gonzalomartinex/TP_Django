d2eb9950b5d53bb525e555162ff6653c7f623212
david <davidandradag@gmail.com>
2023-10-23 08:24:42 -0300

    arreglado url de displaypage y arreglados statics

--- PaginaDeStreaming/Streaming/views.py
+++ PaginaDeStreaming/Streaming/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def frontpage(request):
     return render(request, "frontpage.html")
 # Create your views here.
+
+
+def displaypage(request):
+    return render(request, "displaypage.html")
