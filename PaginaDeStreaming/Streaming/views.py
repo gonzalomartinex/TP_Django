6e8a5bb4d4a0265dbb923908408c4eb01645cc63
david andrada <davidandradag@gmail.com>
2023-11-03 11:56:12 -0300

    cambiado login a register

--- PaginaDeStreaming/Streaming/views.py
+++ PaginaDeStreaming/Streaming/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import *
+from .forms import UsuarioForm
 
 def frontpage(request):
     return render(request, "frontpage.html")
@@ -9,6 +11,16 @@ def frontpage(request):
 def displaypage(request):
     return render(request, "displaypage.html")
 
-def login(request):
-    return render(request, "login.html")
+def register(request):
+
+    form = UsuarioForm
+
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #redirect('home')
+
+    context = {'form': form}
+    return render(request, "register.html", context)
 
